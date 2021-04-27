091aa75c7dffc8ed4f820672a3ca76a3af0d9abb
Alexey Romanov <jgc128@outlook.com>
2021-04-27 02:48:15 +0000

    Use cnn face detector

--- notebooks/test_dlib_face_alignment.py
+++ notebooks/test_dlib_face_alignment.py
@@ -1,12 +1,16 @@
 ### Based on https://github.com/davisking/dlib/blob/master/python_examples/face_alignment.py
 #%%
+import os
+
 import dlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 #%%
-face_detector_path = '/srv/models/dlib/mmod_human_face_detector.dat'
-shape_predictor_path = '/srv/models/dlib/shape_predictor_68_face_landmarks_GTX.dat'
-image_path = '/srv/data/frames/frame_00102.png'
+model_path = '/mnt/batch/tasks/shared/LS_root/mounts/clusters/misha-movie-compute-k80/code/Users/jgc128/dlib-models/'
+data_path = '/mnt/batch/tasks/shared/LS_root/mounts/clusters/misha-movie-compute-k80/code/data/'
+face_detector_path = os.path.join(model_path, 'mmod_human_face_detector.dat')
+shape_predictor_path = os.path.join(model_path, 'shape_predictor_68_face_landmarks_GTX.dat')
+image_path = os.path.join(data_path, 'frames/frame_00065.png')
 #%%
 detector = dlib.cnn_face_detection_model_v1(face_detector_path)
 shape_predictor = dlib.shape_predictor(shape_predictor_path)
@@ -16,16 +20,16 @@ image = dlib.load_rgb_image(image_path) # image.shape (540, 1024, 3)
 detected_faces = detector(image, 1)
 #%%
 plt.imshow(image[
-    detected_faces[0].top():detected_faces[0].bottom(), 
-    detected_faces[0].left():detected_faces[0].right(), 
+    detected_faces[0].rect.top():detected_faces[0].rect.bottom(), 
+    detected_faces[0].rect.left():detected_faces[0].rect.right(), 
     :
 ])
 # %%
 faces = dlib.full_object_detections()
 for detection in detected_faces:
-    faces.append(shape_predictor(image, detection))
+    faces.append(shape_predictor(image, detection.rect))
 #%%
-fig, ax = plt.subplots(figsize=(20, 10))
+fig, ax = plt.subplots(figsize=(10, 5))
 
 ax.imshow(image)
 ax.add_patch(
@@ -39,6 +43,7 @@ for point in faces[0].parts():
         mpatches.Circle(
             (point.x, point.y),
             edgecolor='green', fill=False,
+            radius=1
         )
     )
 
